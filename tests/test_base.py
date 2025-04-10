800d3969edd3f89a39c4f2a56905c89eb2b17048
mhinne@gmail.com <max.hinne@donders.ru.nl>
2025-04-10 17:23:51 +0200

    Add more test cases

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,40 +1,53 @@
 import jax.numpy as jnp
-from bamojax.base import Model, Node
+from bamojax.base import Model
 import distrax as dx
+from jax.scipy.stats import norm
 
 from tensorflow_probability.substrates import jax as tfp
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
-def test_node_shapes():
+y = jnp.asarray([-3.470e-1, -1.520e+0, 6.760e-1, -1.410e+0, -1.610e+0, 1.430e+0, -3.810e-2, 6.930e-1, -8.290e-1,  1.030e+0])
+n = len(y)
 
-    y = jnp.asarray([-3.470e-1, -1.520e+0, 6.760e-1, -1.410e+0, -1.610e+0, 1.430e+0, -3.810e-2, 6.930e-1, -8.290e-1,  1.030e+0])
-    n = len(y)
+model1 = Model('test_model 1')
+A = model1.add_node('A', distribution=dx.Transformed(dx.Normal(loc=0.0, scale=1.0), tfb.Exp()))
+B = model1.add_node('B', distribution=dx.Normal(loc=jnp.zeros((n, )), scale=jnp.ones((n, ))))
+_ = model1.add_node('C', distribution=dx.Normal, observations=y, parents=dict(loc=B, scale=A))
 
-    model1 = Model('test_model 1')
-    node_A = model1.add_node('A', distribution=dx.Transformed(dx.Normal(loc=0.0, scale=1.0), tfb.Exp()))
-    node_B = model1.add_node('B', distribution=dx.Normal(loc=jnp.zeros((n, )), scale=jnp.ones((n, ))))
-    _ = model1.add_node('C', distribution=dx.Normal, observations=y, parents=dict(loc=node_B, scale=node_A))
+model2 = Model('test_model 2')
+D = model2.add_node('D', distribution=dx.Transformed(dx.Normal(loc=0.0, scale=1.0), tfb.Exp()))
+E = model2.add_node('E', distribution=dx.Normal(loc=0.0, scale=1.0), shape=(n, ))
+_ = model2.add_node('F', distribution=dx.Normal, observations=y, parents=dict(loc=E, scale=D))
+
+model3 = Model('test_model 3')
+G = model3.add_node('G', distribution=dx.Transformed(dx.Normal(loc=0.0, scale=1.0), tfb.Exp()))
+H = model3.add_node('H', distribution=dx.Normal(loc=0.0, scale=1.0))
+_ = model3.add_node('I', distribution=dx.Normal, observations=y, parents=dict(loc=H, scale=G))
+
+def test_node_shapes():    
 
     m1 = model1.get_model_size()
     assert m1 == n + 1
 
-    model2 = Model('test_model 2')
-    node_D = model2.add_node('D', distribution=dx.Transformed(dx.Normal(loc=0.0, scale=1.0), tfb.Exp()))
-    node_E = model2.add_node('E', distribution=dx.Normal(loc=0.0, scale=1.0), shape=(n, ))
-    _ = model2.add_node('F', distribution=dx.Normal, observations=y, parents=dict(loc=node_E, scale=node_D))
-
     m2 = model2.get_model_size()
     assert m2 == n + 1
-
-    model3 = Model('test_model 3')
-    node_G = model3.add_node('G', distribution=dx.Transformed(dx.Normal(loc=0.0, scale=1.0), tfb.Exp()))
-    node_H = model3.add_node('H', distribution=dx.Normal(loc=0.0, scale=1.0))
-    _ = model3.add_node('I', distribution=dx.Normal, observations=y, parents=dict(loc=node_H, scale=node_G))
 
     m3 = model3.get_model_size()
     assert m3 == 2
 
+#
+def test_derived_densities():
+
+    A_value = 4.0
+    B_values = jnp.array([-5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0])
+    # Compute log prior (using change-of-variables because we assigned it to log A instead of A directy)
+    prior_density = norm.logpdf(jnp.log(A_value), loc=0.0, scale=1.0) - jnp.log(A_value) + jnp.sum(norm.logpdf(B_values, loc=jnp.zeros((n, )), scale=jnp.ones((n, ))))
+    state = dict(A=A_value, B=B_values)
+    likelihood = jnp.sum(norm.logpdf(y, loc=B_values, scale=A_value))
+    assert likelihood == model1.loglikelihood_fn()(state)
+    assert prior_density == model1.logprior_fn()(state)
+    
 #
 
 
